08c5afd1dd14399535ea280b22f94cbd55bffdf1
nidhukrishna <nidhukrishnag@gmail.com>
2025-06-18 10:27:11 -0400

    Task 1 to 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,9 +5,10 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  // returns true if user already exists
+  return users.some(user => user.username === username);
+};
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,87 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required." });
+    }
+  
+    if (isValid(username)) {
+      return res.status(409).json({ message: "Username already exists." });
+    }
+  
+    users.push({ username, password });
+    return res.status(200).json({ message: "User registered successfully." });
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+  res.status(200).send(JSON.stringify(books, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const matchingBooks = [];
+  
+    for (let isbn in books) {
+      if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+        matchingBooks.push({ isbn, ...books[isbn] });
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found for this author." });
+    }
+  });
+  
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    const matchingBooks = [];
+  
+    for (let isbn in books) {
+      if (books[isbn].title.toLowerCase() === title.toLowerCase()) {
+        matchingBooks.push({ isbn, ...books[isbn] });
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found with this title." });
+    }
+  });
+  
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book.reviews);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
+  
 
 module.exports.general = public_users;
